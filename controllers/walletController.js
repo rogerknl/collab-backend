36007ef715ca1e4007cd009d01a12f59dba7025d
Roger Canela <roger.knl@gmail.com>
2018-06-27 13:36:20 +0200

    style/linted createWallet func

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -2,34 +2,30 @@ const wallet = require (__dirname + '/../services/wallet');
 
 const db = require (__dirname + '/../models/');
 
-  // new wallet has
-  // address:
-  // privateKey:
-  // privateKeyWIF:
 
-exports.createWallet = async (ctx, next) => {
+exports.createWallet = async (ctx) => {
   const newWallet = wallet.createWallet();
 
   let result = await new Promise((resolve,reject)=>{
     db.User.findOne(
-     {where: {username:ctx.user.username},
-       attributes: ['id']
-     }
-   ).then((user)=>{
-     db.Wallet.create({
-         publickey: newWallet.address,
-         privatekey: newWallet.privateKey,
-         wif: newWallet.privateKeyWIF
-       })
-       .then((wallet)=>{
-         db.UserWallet.create({
-           user_id: user.dataValues.id,
-           wallet_id: wallet.dataValues.publickey
-         }).then(()=>resolve('DONE'))
-          .catch((err)=>reject(err));
-       });
-   });
-  })
+      { where: {username:ctx.user.username},
+        attributes: ['id']
+      }
+    ).then((user)=>{
+      db.Wallet.create({
+        publickey: newWallet.address,
+        privatekey: newWallet.privateKey,
+        wif: newWallet.privateKeyWIF
+      })
+        .then((wallet)=>{
+          db.UserWallet.create({
+            user_id: user.dataValues.id,
+            wallet_id: wallet.dataValues.publickey
+          }).then(()=>resolve('DONE'))
+            .catch((err)=>reject(err));
+        });
+    });
+  });
   ctx.jwt.modified = true;
   ctx.body = {result};
 };

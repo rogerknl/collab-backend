7869105ce3da65c397564e48e6b5b0357e563b95
Roger Canela <roger.knl@gmail.com>
2018-06-27 19:45:25 +0200

    feat/add user to wallet

--- controllers/userWalletController.js
+++ controllers/userWalletController.js
@@ -1,16 +1,14 @@
-const wallet = require (__dirname + '/../services/wallet');
-
 
 const db = require (__dirname + '/../models/');
 
 
-exports.usersOfWallet = async ( pk ) => {
+exports.usersOfWallet = async ( publickey ) => {
   let result = await new Promise((resolve,reject) => {
     db.User.findAll({
       include: [{
         model: db.UserWallet,
         where: {
-          wallet_id: pk
+          wallet_id: publickey
         }
       }]
     }).then((users)=>{
@@ -22,4 +20,40 @@ exports.usersOfWallet = async ( pk ) => {
     }).catch((err)=>reject(err));
   });
   return result;
+};
+
+exports.addUserToWallet = async (ctx) => {
+  //check if user to add exists
+  const userToAdd = await db.User.findOne({ where:
+     {username:ctx.request.body.username},
+  attributes: ['id']
+  });
+  if (!userToAdd) return ctx.body = {error: 'User not found'};
+
+  //get id of userAuth
+  const userAuth = await db.User.findOne(
+    { where: {username:ctx.user.username},
+      attributes: ['id']
+    }
+  );
+
+  //check if userAuth has rights over this wallet
+  const uWauth = await db.UserWallet.findOne({
+    where: {user_id: userAuth.id, wallet_id: ctx.request.body.publickey}
+  });
+  if (!uWauth) return ctx.body = {error: 'User has not rights over this Wallet'};
+
+  //check id userToAdd was previously added
+  const alreadyExists = await db.UserWallet.findOne({
+    where: {user_id: userToAdd.id, wallet_id: ctx.request.body.publickey}
+  });
+  if (alreadyExists) return ctx.body = {error: 'User already has rights over this Wallet'};
+
+  //now addding user to add
+  const result = await db.UserWallet.create({
+    user_id: userToAdd.id,
+    wallet_id: ctx.request.body.publickey
+  });
+  if(result) return 'DONE';
+  ctx.body = {error: 'DB error on inserting'};
 };
--- routes.js
+++ routes.js
@@ -4,10 +4,12 @@ const authorize = require(__dirname + '/middleware/auth');
 
 const userCont = require( __dirname + '/controllers/userController' );
 const walletCont = require( __dirname + '/controllers/walletController' );
+const userWalletCont = require( __dirname + '/controllers/userWalletController' );
 const operCont = require( __dirname + '/controllers/operationController' );
 
 router
   .get('/wallet', authorize, walletCont.getWallets)
+  .post('/wallet/add_user',authorize, userWalletCont.addUserToWallet)
   .post('/operations', authorize, operCont.createOperation)
   .post('/wallet', authorize, walletCont.createWallet)
   .post('/login', userCont.signIn)
